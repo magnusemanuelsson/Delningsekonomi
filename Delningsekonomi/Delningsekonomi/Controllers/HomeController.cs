1db0860826d008fe1107491178440fdfc391513d
44tim44 <timeisrelative94@gmail.com>
2018-04-26 13:21:44 +0200

    Added Map

--- Delningsekonomi/Delningsekonomi/Controllers/HomeController.cs
+++ Delningsekonomi/Delningsekonomi/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using GMapsAPITest;
+using GMapsAPITest.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +11,28 @@ namespace Delningsekonomi.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private PointsAPI service = new PointsAPI();
+
+        public async Task<ActionResult> Index()
         {
-            return View();
+
+            PointJSON pointsList = await service.GetPoints("63.8181", "20.3073", "1000", null);
+            string jsonString = "{\"type\": \"FeatureCollection\",\"features\": [";
+
+            foreach (var item in pointsList.resources)
+            {
+                jsonString = jsonString + "{\"geometry\": {\"type\": \"Point\",\"coordinates\": [" + item.Location.Longitude.ToString() + "," + item.Location.Latitude.ToString() + "]},";
+                jsonString = jsonString + "\"type\": \"Feature\",\"properties\": { ";
+                jsonString = jsonString + "\"category\": \"FoodPurchaseOffered\",\"hours\": \"N/A\",\"description\": \"" + ((item.Description != null) ? item.Description.ToString() : "") + "\",\"name\": \"" + ((item.Title != null) ? item.Title.ToString() : "") + "\",\"phone\": \"N/A\"";
+                jsonString = jsonString + "}},";
+            }
+            jsonString = jsonString.TrimEnd(',');
+            jsonString = jsonString + "]}";
+            System.Diagnostics.Debug.WriteLine(jsonString);
+            System.Diagnostics.Debug.WriteLine("Hej");
+            ViewBag.APIstring = jsonString;
+
+            return View("Index", model: jsonString);
         }
 
         public ActionResult About()
---
+++ Delningsekonomi/Delningsekonomi/PointsAPI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using GMapsAPITest.Models;
+using Newtonsoft.Json;
+
+namespace GMapsAPITest
+{
+    public class PointsAPI
+    {
+
+        readonly string uri = "https://www.askvigg.com/api/v1.1/resources.json";
+
+        public async Task<PointJSON> GetPoints(string latitude, string longitude, string distance, string tags)
+        {
+            if(distance == null)
+            {
+                distance = "";
+            }
+            if (tags == null)
+            {
+                tags = "";
+            }
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string apistr = await httpClient.GetStringAsync(uri + "?latitude=" + latitude + "&longitude=" + longitude + "&distance=" + distance + "&tags=" + tags);
+                var returnval = JsonConvert.DeserializeObject<PointJSON>(apistr);
+                return returnval;
+                
+                /*
+                dynamic dyn = JsonConvert.DeserializeObject(await httpClient.GetStringAsync(uri + "?latitude=" + latitude + "&longitude=" + longitude + "&distance=" + distance + "&tags=" + tags));
+                var modelPoint = new List<Point>();
+
+                foreach (var obj in dyn.data)
+                {
+                    modelPoint.Add(new Point()
+                    {
+                        Title = (obj.title != null) ? obj.title.ToString() : "",
+                        Latitude = (obj.location.latitude != null) ? obj.location.latitude.ToString() : "",
+                        Longitude = (obj.location.longitude != null) ? obj.location.longitude.ToString() : "",
+                        Tags = (obj.tags != null) ? obj.tags.ToList() : "",
+                        Description = (obj.description != null) ? obj.description.ToString() : ""
+                    });
+                }
+
+                return modelPoint;
+                */
+            }
+        }
+    }
+}
