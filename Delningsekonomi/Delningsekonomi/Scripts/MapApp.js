fc821ef64b0f0e2139b9746f192eb04bdaf4c2d4
Mágnus <oi14mes@cs.umu.se>
2018-05-14 14:02:26 +0200

    nya ändringar

--- Delningsekonomi/Delningsekonomi/Scripts/MapApp.js
+++ Delningsekonomi/Delningsekonomi/Scripts/MapApp.js
@@ -119,6 +119,7 @@ function initialize() {
     initAutocomplete();
 }
 
+
 // Escapes HTML characters in a template literal string, to prevent XSS.
 // See https://www.owasp.org/index.php/XSS_%28Cross_Site_Scripting%29_Prevention_Cheat_Sheet#RULE_.231_-_HTML_Escape_Before_Inserting_Untrusted_Data_into_HTML_Element_Content
 function sanitizeHTML(strings) {
@@ -261,4 +262,23 @@ function initAutocomplete() {
         google.maps.event.addDomListener(window, 'load', initialize);
         document.getElementById("form").submit();
     });
+    
+}
+function getDistance() {
+
+    var rad = function (x) {
+        return x * Math.PI / 180;
+    };
+
+    var getDistance = function (p1, p2) {
+        var R = 6378137; // Earth’s mean radius in meter
+        var dLat = rad(p2.lat - p1.lat);
+        var dLong = rad(p2.lng - p1.lng);
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
+            Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        return d; // returns the distance in meter
+    };
 }
